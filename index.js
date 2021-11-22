ab1009eb8c18033d105914ce7a18c4edc3d98b1e
arifinilhamtea <imhmd190723@gmail.com>
2021-11-22 21:38:47 +0700

    perubahan kedua

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const nama = "INTAN MEILANI";
 let hobi = "maen bola";
-hobi = "married";
+hobi = "dia";
 
-console.log(`saya sangat cinta kepada ${nama} karena itu ayo kita ${hobi}`);
+console.log(`saya suka ${nama} terkadang saya ingin menendang ${hobi}`);
 
 
 
